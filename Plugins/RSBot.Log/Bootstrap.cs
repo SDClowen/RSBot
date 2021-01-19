85e8dbeb5d7c5c93d1617ecd7e47e31e54533f67
SDClowen <mahmutyildirim@outlook.com.tr>
2021-01-19 23:39:36 +0300

    * Added log plugin

---
+++ Plugins/RSBot.Log/Bootstrap.cs
@@ -0,0 +1,47 @@
+﻿using RSBot.Core.Plugins;
+using System.Windows.Forms;
+
+namespace RSBot.Log
+{
+    public class Bootstrap : IPlugin
+    {
+        /// <summary>
+        /// Created log component control
+        /// </summary>
+        private Views.Main _control;
+
+        /// <summary>
+        /// Gets or sets the information of the plugin.
+        /// </summary>
+        /// <value>
+        /// The information.
+        /// </value>
+        public PluginInfo Information => new PluginInfo
+        {
+            DisplayAsTab = true,
+            DisplayName = "Log",
+            InternalName = "RSBot.Log",
+            LoadIndex = 0,
+            TabDisplayIndex = 100,
+            RequireIngame = false
+        };
+
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
+        public void Initialize()
+        {
+            _control = new Views.Main();
+
+            Core.Log.Notify("Plugin [Log] initialized!");
+        }
+
+        /// <summary>
+        /// Gets the view that will be displayed as tab page.
+        /// </summary>
+        public Control GetView()
+        {
+            return _control;
+        }
+    }
+}
---
+++ Plugins/RSBot.Log/Views/Main.cs
@@ -0,0 +1,101 @@
+﻿using RSBot.Core;
+using RSBot.Core.Event;
+using RSBot.Theme.Extensions;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RSBot.Log.Views
+{
+    [System.ComponentModel.ToolboxItem(false)]
+    public partial class Main : UserControl
+    {
+        /// <summary>
+        /// Log file path
+        /// </summary>
+        private static string _filePath;
+
+        /// <summary>
+        /// Is active debug logs <seealso cref="true"/> otherwise <seealso cref="false"/>
+        /// </summary>
+        private bool _debug;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Main"/> class.
+        /// </summary>
+        public Main()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+            LoadConfig();
+
+            _filePath = Path.Combine(Environment.CurrentDirectory, "User", "Logs", $"{DateTime.Now:dd-MM-yyyy}.txt");
+            _debug = GlobalConfig.Get<bool>("RSBot.DebugEnvironment");
+
+            EventManager.SubscribeEvent("OnAddLog", new Action<string, LogLevel>(AppendLog));
+
+            AppendLog($"Logfile: {_filePath}");
+
+            if(!_debug)
+            {
+                checkDebug.Checked = false;
+                checkError.Visible = false;
+                checkNormal.Visible = false;
+                checkWarning.Visible = false;
+                checkDebug.Visible = false;
+            }
+        }
+
+        /// <summary>
+        /// Appends the log.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public void AppendLog(string message, LogLevel level  = LogLevel.Notify)
+        {
+            if (!checkEnabled.Checked) 
+                return;
+
+            if (!_debug && level == LogLevel.Debug && !checkDebug.Checked) 
+                return;
+
+            if (level == LogLevel.Error && !checkError.Checked) 
+                return;
+
+            if (level == LogLevel.Notify && !checkNormal.Checked) 
+                return;
+
+            if (level == LogLevel.Warning && !checkWarning.Checked)
+                return;
+
+            txtLog.Write($"<{level}> \t{message}", true, _debug, _filePath);
+        }
+
+        /// <summary>
+        /// Loads the configuration.
+        /// </summary>
+        private void LoadConfig()
+        {
+            checkEnabled.Checked = GlobalConfig.Get<bool>("RSBot.Log.logEnabled", true);
+        }
+
+        /// <summary>
+        /// Handles the CheckedChanged event of the checkEnabled control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void checkEnabled_CheckedChanged(object sender, EventArgs e)
+        {
+            GlobalConfig.Set("RSBot.Log.logEnabled", checkEnabled.Checked.ToString());
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnReset control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtLog.Clear();
+        }
+    }
+}
