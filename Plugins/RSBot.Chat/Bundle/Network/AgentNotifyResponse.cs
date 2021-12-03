ec3cac1d7dcf98bb01dbbc64f489fdd0548402cc
SDClowen <mahmutyildirim@outlook.com.tr>
2021-12-03 13:41:43 +0200

    Added unique spawn/dead/disappeared logs

---
+++ Plugins/RSBot.Chat/Bundle/Network/AgentNotifyResponse.cs
@@ -0,0 +1,67 @@
+﻿using RSBot.Core;
+using RSBot.Core.Network;
+using RSBot.Theme.Extensions;
+
+namespace RSBot.Chat.Network
+{
+    internal class AgentNotifyResponse : IPacketHandler
+    {
+        /// <summary>
+        /// Gets or sets the opcode.
+        /// </summary>
+        /// <value>
+        /// The opcode.
+        /// </value>
+        public ushort Opcode => 0x300C;
+
+        /// <summary>
+        /// Gets or sets the destination.
+        /// </summary>
+        /// <value>
+        /// The destination.
+        /// </value>
+        public PacketDestination Destination => PacketDestination.Client;
+
+        /// <summary>
+        /// Handles the packet.
+        /// </summary>
+        /// <param name="packet">The packet.</param>
+        public void Invoke(Packet packet)
+        {
+            var noticeType = packet.ReadUShort();
+            switch(noticeType)
+            {
+                //[%s] has appeared
+                case 0xC05:
+
+                    var refObjId = packet.ReadUInt();
+                    if (!Game.ReferenceManager.CharacterData.TryGetValue(refObjId, out var obj))
+                        return;
+
+                    Views.View.Instance.UniqueText.Write($"{obj.GetRealName()} has appeared.");
+
+                    break;
+
+                //[%s] has disappeared or killed
+                case 0xC06:
+
+                    refObjId = packet.ReadUInt();
+                    if (!Game.ReferenceManager.CharacterData.TryGetValue(refObjId, out obj))
+                        return;
+
+                    var characterName = packet.ReadString();
+
+                    // If name equals "???" then "[%s] has disappeared." is displayed.
+                    if (characterName == "???")
+                    {
+                        Views.View.Instance.UniqueText.Write($"{obj.GetRealName()} has disappeared.");
+                        return;
+                    }
+
+                    Views.View.Instance.UniqueText.Write($"{characterName} has killed the {obj.GetRealName()}.");
+
+                    break;
+            }
+        }
+    }
+}
