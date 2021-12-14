9e2172f97b1f8e3df337757aa42544a048f82325
tuthienloc92bk <39431314+tuthienloc92bk@users.noreply.github.com>
2021-12-14 14:09:02 +0700

    Update ReferenceManager.cs
    Fix "No results ..." when search with "Accessories " && "Gender Male/Female" option.

--- Library/RSBot.Core/Client/ReferenceManager.cs
+++ Library/RSBot.Core/Client/ReferenceManager.cs
@@ -480,7 +480,7 @@ namespace RSBot.Core.Client
                         if (refItem.Degree >= degreeFrom && refItem.Degree <= degreeTo || degreeTo == 0)
                         {
                             var itemGender = (ObjectGender)refItem.ReqGender;
-                            if (itemGender == gender || gender == ObjectGender.Neutral)
+                            if (itemGender == gender || gender == ObjectGender.Neutral||refItem.TypeID3 == 12||refItem.TypeID3 == 5)
                             {
                                 if (!string.IsNullOrEmpty(searchPattern))
                                 {
