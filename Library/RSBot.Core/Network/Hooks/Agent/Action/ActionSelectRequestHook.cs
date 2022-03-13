af47871958bcc817db990ec5851d05f2fd986b84
SDClowen <mahmutyildirim@outlook.com.tr>
2022-03-14 02:24:36 +0300

    Block action select request by user while bot running

---
+++ Library/RSBot.Core/Network/Hooks/Agent/Action/ActionSelectRequestHook.cs
@@ -0,0 +1,34 @@
+﻿namespace RSBot.Core.Network.Hooks.Agent.Action
+{
+    internal class ActionSelectRequestHook : IPacketHook
+    {
+        /// <summary>
+        /// Gets the opcode.
+        /// </summary>
+        /// <value>
+        /// The opcode.
+        /// </value>
+        public ushort Opcode => 0x7045;
+
+        /// <summary>
+        /// Gets the destination.
+        /// </summary>
+        /// <value>
+        /// The destination.
+        /// </value>
+        public PacketDestination Destination => PacketDestination.Server;
+
+        /// <summary>
+        /// Replaces the packet and returns a new packet.
+        /// </summary>
+        /// <param name="packet"></param>
+        /// <returns></returns>
+        public Packet ReplacePacket(Packet packet)
+        {
+            if (Kernel.Bot.Running)
+                return null;
+
+            return packet;
+        }
+    }
+}
