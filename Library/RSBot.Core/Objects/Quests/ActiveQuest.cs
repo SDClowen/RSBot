e0e29df0c80e12ba86361d81781efb05ce0a7b88
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2022-03-24 09:04:30 +0300

    Quest Parsing converted to object. (#105)

---
+++ Library/RSBot.Core/Objects/Quests/ActiveQuest.cs
@@ -0,0 +1,18 @@
+﻿namespace RSBot.Core.Objects.Quests
+{
+    public struct ActiveQuest
+    {
+        public int QuestId;
+        public int AchievementAmount;
+        public int RequiredShareParty;
+
+        public byte Unknown1;
+        public byte Unknown2;
+
+        public byte Type;
+        public int RemainingTime;
+        public byte State;
+        public QuestObjective[] Objectives;
+        public uint[] Npcs;
+    }
+}
