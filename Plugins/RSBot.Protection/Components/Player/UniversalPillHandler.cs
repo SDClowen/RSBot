04191c9111db33979bea5848cb938f1003dda72c
Dariusz Figzał <dariuszfigzal@gmail.com>
2022-05-03 04:30:28 +0200

    Fixed issue #5 (#238)

--- Plugins/RSBot.Protection/Components/Player/UniversalPillHandler.cs
+++ Plugins/RSBot.Protection/Components/Player/UniversalPillHandler.cs
@@ -1,5 +1,6 @@
 ﻿using RSBot.Core;
 using RSBot.Core.Event;
+using RSBot.Core.Objects;
 
 namespace RSBot.Protection.Components.Player
 {
@@ -18,8 +19,38 @@ namespace RSBot.Protection.Components.Player
         /// </summary>
         private static void SubscribeEvents()
         {
-            EventManager.SubscribeEvent("OnPlayerBadEffect", OnPlayerBadEffect);
+            EventManager.SubscribeEvent("OnTick", OnPlayerBadEffect);
         }
+
+        /// <summary>
+        /// Bad effects curable by universal pills.
+        /// </summary>
+        private static readonly BadEffect universalPillEffects =
+            BadEffect.Frozen |
+            BadEffect.Frostbitten |
+            BadEffect.Shocked |
+            BadEffect.Burnt |
+            BadEffect.Poisoned |
+            BadEffect.Zombie;
+
+        /// <summary>
+        /// Bad effects curable by purification pills.
+        /// </summary>
+        private static readonly BadEffect purificationPillEffects =
+            BadEffect.Sleep |
+            BadEffect.Bind |
+            BadEffect.Dull |
+            BadEffect.Fear |
+            BadEffect.ShortSighted |
+            BadEffect.Bleed |
+            BadEffect.Darkness |
+            BadEffect.Disease |
+            BadEffect.Decay |
+            BadEffect.Weak |
+            BadEffect.Impotent |
+            BadEffect.Division |
+            BadEffect.Panic |
+            BadEffect.Hidden;
 
         /// <summary>
         /// Cores the on player bad effect.
@@ -29,7 +60,16 @@ namespace RSBot.Protection.Components.Player
             var useUniversalPill = PlayerConfig.Get<bool>("RSBot.Protection.checkUseUniversalPills", true);
 
             if (useUniversalPill)
-                Game.Player.UseUniversalPill();
+            {
+                if ((Game.Player.BadEffect & universalPillEffects) != 0)
+                {
+                    Game.Player.UseUniversalPill();
+                }
+                else if ((Game.Player.BadEffect & purificationPillEffects) != 0)
+                {
+                    Game.Player.UsePurificationPill();
+                }
+            }
         }
     }
 }
