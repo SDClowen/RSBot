73d9f09e9875379f96780ae11c10b85644655c98
Niklas Gödde <nigoedde@gmail.com>
2022-05-24 02:11:58 +0200

    [fix] Vehicle state for other players (#265)
    Co-authored-by: Niklas Gödde <niklas.goedde@mediabeam.com>

---
+++ Library/RSBot.Core/Network/Handler/Agent/Cos/UpdateMountStateResponse.cs
@@ -0,0 +1,57 @@
+﻿using RSBot.Core.Components;
+using RSBot.Core.Event;
+using RSBot.Core.Objects.Spawn;
+
+namespace RSBot.Core.Network.Handler.Agent.Cos
+{
+    internal class UpdateMountStateResponse : IPacketHandler
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the opcode.
+        /// </summary>
+        /// <value>
+        /// The opcode.
+        /// </value>
+        public ushort Opcode => 0xB0CB;
+
+        /// <summary>
+        /// Gets or sets the destination.
+        /// </summary>
+        /// <value>
+        /// The destination.
+        /// </value>
+        public PacketDestination Destination => PacketDestination.Client;
+
+        #endregion Properties
+
+        #region Methods
+
+        public void Invoke(Packet packet)
+        {
+            if (packet.ReadByte() != 0x01)
+                return;
+
+            var ownerUId = packet.ReadUInt();
+            var isMounted = packet.ReadBool();
+            var cosUid = packet.ReadUInt();
+
+            //Assertion: only player's are supported to have active vehicles. Think it's the same in the client.
+            var owner = SpawnManager.GetEntity<SpawnedPlayer>(e => e.UniqueId == ownerUId);
+            if (owner == null)
+                return;
+
+            var cos = SpawnManager.GetEntity<SpawnedCos>(e => e.UniqueId == cosUid);
+            if (cos == null)
+                return;
+
+            owner.OnTransport = isMounted;
+            owner.TransportUniqueId = cosUid;
+
+            EventManager.FireEvent("OnEntityMountTransport", owner, cos, isMounted);
+        }
+
+        #endregion Methods
+    }
+}
