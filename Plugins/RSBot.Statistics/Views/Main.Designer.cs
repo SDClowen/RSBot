0a7946028a966a4055b5bbc48e453c9b68f26068
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2022-06-12 16:50:33 +0300

    Temprorary fix for nullreference expcetion in statistics plugin (#301)

--- Plugins/RSBot.Statistics/Views/Main.Designer.cs
+++ Plugins/RSBot.Statistics/Views/Main.Designer.cs
@@ -169,7 +169,7 @@
             // 
             this.timer.Enabled = true;
             this.timer.Interval = 1000;
-            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            this.timer.Tick += new System.EventHandler(this.RefreshTimer_Elapsed);
             // 
             // Main
             // 
