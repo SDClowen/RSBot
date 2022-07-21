bbeed690a87ed858dafd4f6e5f2255a38f2d14a0
SDClowen <mahmutyildirim@outlook.com.tr>
2022-07-22 02:03:57 +0300

    Add feature, for use extra ability item skills (Skill Name: Mirror Reflect)

---
+++ Library/RSBot.Core/Client/ReferenceObjects/RefExtraAbilityByEquipItemOptLevel.cs
@@ -0,0 +1,23 @@
+﻿namespace RSBot.Core.Client.ReferenceObjects
+{
+    public class RefExtraAbilityByEquipItemOptLevel : IReference
+    {
+        public uint ItemId;
+        public byte OptLevel;
+        public uint SkillId;
+
+        public bool Load(ReferenceParser parser)
+        {
+            if (!parser.TryParse(0, out int service) || service == 0)
+                return false;
+
+            if (!parser.TryParse(20, out SkillId) || SkillId == 0)
+                return false;
+
+            parser.TryParse(1, out ItemId);
+            parser.TryParse(2, out OptLevel);
+
+            return true;
+        }
+    }
+}
