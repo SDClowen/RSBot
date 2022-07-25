e14636a3c9a8b6409602e8b1fdebe1cd7af3087e
SDClowen <mahmutyildirim@outlook.com.tr>
2022-07-25 05:48:24 +0300

    Fixed issue #331

--- Application/RSBot/Helper/ProfilePathHelper.cs
+++ Application/RSBot/Helper/ProfilePathHelper.cs
@@ -7,17 +7,17 @@ namespace RSBot.Helper
     {
         public static string GetProfileConfigFileName()
         {
-            return Path.Combine(Environment.CurrentDirectory, "User", "Profiles.rs");
+            return Path.Combine(Environment.CurrentDirectory, "Data", "User", "Profiles.rs");
         }
 
         public static string GetProfileFile(string profileName)
         {
-            return Path.Combine(Environment.CurrentDirectory, "User", $"{profileName}.rs");
+            return Path.Combine(Environment.CurrentDirectory, "Data", "User", $"{profileName}.rs");
         }
 
         public static string GetProfileDirectory(string profileName)
         {
-            return Path.Combine(Environment.CurrentDirectory, "User", profileName);
+            return Path.Combine(Environment.CurrentDirectory, "Data", "User", profileName);
         }
     }
 }
