c04beb8e5200ac06fa5b368b36e1a46787bc708f
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2022-09-09 03:17:57 +0300

    Fixed issue #391 (#399)

---
+++ Botbases/RSBot.Default/Bundle/Protection/ManaRecovery.cs
@@ -0,0 +1,12 @@
+﻿using RSBot.Core;
+
+namespace RSBot.Default.Bundle.Protection
+{
+    internal class ManaRecovery
+    {
+        public static bool Active => PlayerConfig.Get<bool>("RSBot.Protection.checkUseSkillMP");
+        public static int Value => PlayerConfig.Get("RSBot.Protection.numPlayerSkillMPMin", 50);
+        public static uint SkillId => PlayerConfig.Get<uint>("RSBot.Protection.MpSkill");
+        public static double Current => 100.0 * Game.Player.Mana / Game.Player.MaximumMana;
+    }
+}
