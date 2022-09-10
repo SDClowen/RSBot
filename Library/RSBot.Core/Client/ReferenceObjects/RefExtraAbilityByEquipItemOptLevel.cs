13754bab4108c041047c246fff85331376585beb
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2022-09-10 03:50:17 +0300

    The extra ability skills shown was only one skill. Now its parsing all. (#410)

--- Library/RSBot.Core/Client/ReferenceObjects/RefExtraAbilityByEquipItemOptLevel.cs
+++ Library/RSBot.Core/Client/ReferenceObjects/RefExtraAbilityByEquipItemOptLevel.cs
@@ -2,20 +2,28 @@
 {
     public class RefExtraAbilityByEquipItemOptLevel : IReference
     {
+        private const int SKILL_COUNT = 5;
+
         public uint ItemId;
         public byte OptLevel;
-        public uint SkillId;
+        public uint[] Skills;
 
         public bool Load(ReferenceParser parser)
         {
             if (!parser.TryParse(0, out int service) || service == 0)
                 return false;
 
-            if (!parser.TryParse(20, out SkillId) || SkillId == 0)
-                return false;
-
             parser.TryParse(1, out ItemId);
             parser.TryParse(2, out OptLevel);
+
+            Skills = new uint[SKILL_COUNT];
+            for (int i = 0; i < SKILL_COUNT; i++)
+            {
+                if (!parser.TryParse(20 + i, out uint skillId))
+                    return false;
+
+                Skills[i] = skillId;
+            }
 
             return true;
         }
