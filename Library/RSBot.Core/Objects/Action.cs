ea74c2b9ac16db36e2d96e782c12ec531f62324e
SDClowen <mahmutyildirim@outlook.com.tr>
2022-10-06 04:19:04 +0300

    Fixed some parsing errors on isro, trsro, ksro

--- Library/RSBot.Core/Objects/Action.cs
+++ Library/RSBot.Core/Objects/Action.cs
@@ -109,11 +109,23 @@ namespace RSBot.Core.Objects
                 action.UnknownId = packet.ReadUInt();
 
             action.TargetId = packet.ReadUInt();
+            if (Game.ClientType == GameClientType.Global)
+            {
+                packet.ReadByte();
+                action.Flag = (ActionStateFlag)packet.ReadByte();
+            }
+            else if (Game.ClientType == GameClientType.Turkey)
+            {
+                action.Flag = (ActionStateFlag)packet.ReadByte();
+                packet.ReadByte();
+            }
+            else
+                action.Flag = (ActionStateFlag)packet.ReadByte();
 
-            if (Game.ClientType >= GameClientType.Global)
+            /*if (Game.ClientType >= GameClientType.Global)
                 packet.ReadByte();
 
-            action.Flag = (ActionStateFlag)packet.ReadByte();
+            action.Flag = (ActionStateFlag)packet.ReadByte();*/
             action.SerializeDetail(packet);
 
             return action;
