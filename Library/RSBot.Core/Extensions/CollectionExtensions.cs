52a9df9130f7dfd3ff5c3b22da9988032630f246
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2022-10-22 02:43:50 +0300

    Optimized targetbundle on botbase (#522)

---
+++ Library/RSBot.Core/Extensions/CollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RSBot.Core.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static void RemoveAll<K, V>(this IDictionary<K, V> dict, Func<K, V, bool> predicate)
+        {
+            foreach (var key in dict.Keys.ToArray().Where(key => predicate(key, dict[key])))
+                dict.Remove(key);
+        }
+    }
+}
