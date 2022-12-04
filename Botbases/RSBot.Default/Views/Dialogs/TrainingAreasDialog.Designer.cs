90422c3c7b00f4a8deb3a19f9b22492bd69db370
SDClowen <mahmutyildirim@outlook.com.tr>
2022-12-05 02:29:57 +0300

    Remove saved training area from training area window

--- Botbases/RSBot.Default/Views/Dialogs/TrainingAreasDialog.Designer.cs
+++ Botbases/RSBot.Default/Views/Dialogs/TrainingAreasDialog.Designer.cs
@@ -39,9 +39,10 @@
             this.columnHeaderR = new System.Windows.Forms.ColumnHeader();
             this.columnHeaderS = new System.Windows.Forms.ColumnHeader();
             this.panel1 = new SDUI.Controls.Panel();
+            this.labelPos = new SDUI.Controls.Label();
             this.buttonCancel = new SDUI.Controls.Button();
             this.buttonAccept = new SDUI.Controls.Button();
-            this.labelPos = new SDUI.Controls.Label();
+            this.removeSelectedAreaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -49,15 +50,16 @@
             // contextMenuStrip
             // 
             this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.createToolStripMenuItem});
+            this.createToolStripMenuItem,
+            this.removeSelectedAreaToolStripMenuItem});
             this.contextMenuStrip.Name = "contextMenuStrip";
-            this.contextMenuStrip.Size = new System.Drawing.Size(109, 26);
+            this.contextMenuStrip.Size = new System.Drawing.Size(189, 70);
             // 
             // createToolStripMenuItem
             // 
             this.createToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.createToolStripMenuItem.Name = "createToolStripMenuItem";
-            this.createToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
             this.createToolStripMenuItem.Text = "Create";
             this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
             // 
@@ -77,7 +79,7 @@
             this.listView.FullRowSelect = true;
             this.listView.Location = new System.Drawing.Point(1, 1);
             this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(617, 281);
+            this.listView.Size = new System.Drawing.Size(613, 277);
             this.listView.TabIndex = 1;
             this.listView.UseCompatibleStateImageBehavior = false;
             this.listView.View = System.Windows.Forms.View.Details;
@@ -121,19 +123,29 @@
             this.panel1.Controls.Add(this.buttonCancel);
             this.panel1.Controls.Add(this.buttonAccept);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(1, 282);
+            this.panel1.Location = new System.Drawing.Point(1, 278);
             this.panel1.Name = "panel1";
             this.panel1.Radius = 0;
             this.panel1.ShadowDepth = 0F;
-            this.panel1.Size = new System.Drawing.Size(617, 39);
+            this.panel1.Size = new System.Drawing.Size(613, 39);
             this.panel1.TabIndex = 2;
+            // 
+            // labelPos
+            // 
+            this.labelPos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelPos.Location = new System.Drawing.Point(203, 1);
+            this.labelPos.Name = "labelPos";
+            this.labelPos.Size = new System.Drawing.Size(234, 36);
+            this.labelPos.TabIndex = 1;
+            this.labelPos.Text = "Experimental Window\r\nRight Click for create new";
+            this.labelPos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // buttonCancel
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.Color = System.Drawing.Color.Transparent;
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(530, 8);
+            this.buttonCancel.Location = new System.Drawing.Point(526, 8);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Radius = 6;
             this.buttonCancel.ShadowDepth = 4F;
@@ -157,15 +169,13 @@
             this.buttonAccept.UseVisualStyleBackColor = true;
             this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
             // 
-            // labelPos
+            // removeSelectedAreaToolStripMenuItem
             // 
-            this.labelPos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.labelPos.Location = new System.Drawing.Point(203, 1);
-            this.labelPos.Name = "labelPos";
-            this.labelPos.Size = new System.Drawing.Size(234, 36);
-            this.labelPos.TabIndex = 1;
-            this.labelPos.Text = "Experimental Window\r\nRight Click for create new";
-            this.labelPos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.removeSelectedAreaToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.removeSelectedAreaToolStripMenuItem.Name = "removeSelectedAreaToolStripMenuItem";
+            this.removeSelectedAreaToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.removeSelectedAreaToolStripMenuItem.Text = "Remove selected area";
+            this.removeSelectedAreaToolStripMenuItem.Click += new System.EventHandler(this.removeSelectedAreaToolStripMenuItem_Click);
             // 
             // TrainingAreasDialog
             // 
@@ -173,7 +183,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(619, 322);
+            this.ClientSize = new System.Drawing.Size(615, 318);
             this.ControlBox = false;
             this.Controls.Add(this.listView);
             this.Controls.Add(this.panel1);
@@ -209,5 +219,6 @@
         private SDUI.Controls.Button buttonAccept;
         private System.Windows.Forms.ColumnHeader columnHeaderRegion;
         private SDUI.Controls.Label labelPos;
+        private System.Windows.Forms.ToolStripMenuItem removeSelectedAreaToolStripMenuItem;
     }
 }
