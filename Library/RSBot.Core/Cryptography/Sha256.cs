11d446c1f96183cfb4791b72225d7874c3543737
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2023-02-03 15:57:19 +0300

    Fixed isro, ksro, trsro C9 erorr

---
+++ Library/RSBot.Core/Cryptography/Sha256.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace RSBot.Core.Cryptography
+{
+    public class Sha256
+    {
+        public static string ComputeHash(string rawData)
+        {
+            using (var sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                StringBuilder builder = new();
+                for (int i = 0; i < bytes.Length; i++)
+                    builder.Append(bytes[i].ToString("X2"));
+
+                return builder.ToString();
+            }
+        }
+    }
+}
