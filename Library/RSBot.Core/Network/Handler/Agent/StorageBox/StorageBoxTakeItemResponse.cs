ac054b3f1a11843d50153d0e9af63177b80f74e0
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2023-03-18 13:02:43 +0300

    Fix detecting when got any item on item storage box window [isro, csror, rigid, trsro]

---
+++ Library/RSBot.Core/Network/Handler/Agent/StorageBox/StorageBoxTakeItemResponse.cs
@@ -0,0 +1,41 @@
+﻿using RSBot.Core.Event;
+using RSBot.Core.Objects;
+
+namespace RSBot.Core.Network.Handler.Agent.StorageBox
+{
+    public class StorageBoxTakeItemResponse : IPacketHandler
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public ushort Opcode => 0xB558;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public PacketDestination Destination => PacketDestination.Client;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="packet"></param>
+        public void Invoke(Packet packet)
+        {
+            var result = packet.ReadBool();
+            if (!result)
+                return;
+
+            var count = packet.ReadInt();
+            for (int i = 0; i < count; i++)
+            {
+                var item = InventoryItem.FromPacket(packet);
+                if (item == null)
+                    continue;
+
+                Game.Player.Inventory.Add(item);
+            }
+
+            EventManager.FireEvent("OnInventoryUpdate");
+        }
+    }
+}
