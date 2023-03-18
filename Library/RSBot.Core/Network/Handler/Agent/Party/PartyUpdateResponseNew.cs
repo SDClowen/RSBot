ca2ea9505377285a44c81ae12ae1872f83e75ba7
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2023-03-18 21:06:18 +0300

    Fix null reference exception when player join to active party (#611)

---
+++ Library/RSBot.Core/Network/Handler/Agent/Party/PartyUpdateResponseNew.cs
@@ -0,0 +1,30 @@
+﻿namespace RSBot.Core.Network.Handler.Agent.Party
+{
+    internal class PartyUpdateResponseNew : IPacketHandler
+    {
+        /// <summary>
+        /// Gets or sets the opcode.
+        /// </summary>
+        /// <value>
+        /// The opcode.
+        /// </value>
+        public ushort Opcode => 0x3E6E;
+
+        /// <summary>
+        /// Gets or sets the destination.
+        /// </summary>
+        /// <value>
+        /// The destination.
+        /// </value>
+        public PacketDestination Destination => PacketDestination.Client;
+
+        /// <summary>
+        /// Handles the packet.
+        /// </summary>
+        /// <param name="packet">The packet.</param>
+        public void Invoke(Packet packet)
+        {
+            PartyUpdateResponse.PartyUpdateResponseCommon(packet);
+        }
+    }
+}
