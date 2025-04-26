f4b3f114c7b6bde2e499012d8e9e2e0ab0421417
kis1yi <kiselbox@gmail.com>
2025-04-26 17:33:39 +0200

    Cast skills on the Protection tab only if they are inactive

--- Botbases/RSBot.Default/Bundle/Protection/ProtectionBundle.cs
+++ Botbases/RSBot.Default/Bundle/Protection/ProtectionBundle.cs
@@ -8,22 +8,28 @@ internal class ProtectionBundle : IBundle
 {
     public void Invoke()
     {
-        if (HealthRecovery.Active && HealthRecovery.Value > HealthRecovery.Current)
+        if (HealthRecovery.Active && HealthRecovery.Value > HealthRecovery.Current && !IsActive(HealthRecovery.SkillId))
             if ((Game.Player.BadEffect & BadEffect.Zombie) != BadEffect.Zombie) // is it need?
                 SkillManager.CastBuff(Game.Player.Skills.GetSkillInfoById(HealthRecovery.SkillId));
 
-        if (ManaRecovery.Active && ManaRecovery.Value > ManaRecovery.Current)
+        if (ManaRecovery.Active && ManaRecovery.Value > ManaRecovery.Current && !IsActive(ManaRecovery.SkillId))
             SkillManager.CastBuff(Game.Player.Skills.GetSkillInfoById(ManaRecovery.SkillId));
 
 
         if (BadStateRecovery.Active)
         {
-            if (BadStateRecovery.IsUniversall)
+            if (BadStateRecovery.IsUniversall && !IsActive(BadStateRecovery.SkillIdForUniversall))
                 SkillManager.CastBuff(Game.Player.Skills.GetSkillInfoById(BadStateRecovery.SkillIdForUniversall));
 
-            if (BadStateRecovery.IsPurification)
+            if (BadStateRecovery.IsPurification && !IsActive(BadStateRecovery.SkillIdForPurification))
                 SkillManager.CastBuff(Game.Player.Skills.GetSkillInfoById(BadStateRecovery.SkillIdForPurification));
         }
+    }
+
+    private bool IsActive(uint skillId)
+    {
+        var skill = Game.Player.Skills.GetSkillInfoById(skillId);
+        return Game.Player.State.HasActiveBuff(skill, out _);
     }
 
     public void Refresh()
